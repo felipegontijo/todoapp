93a5565805b6b5f0a8ae45e2997789d25213c14a
Felipe Gontijo <epilef.gontijo@gmail.com>
2020-10-14 14:55:13 -0300

    feat: Feed data through Postgres database

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://felipegontijo@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo ID: {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', list=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3' 
-    }])
+    return render_template('index.html', todos = Todo.query.all())
 
 if __name__ == '__main__':
     app.run()
