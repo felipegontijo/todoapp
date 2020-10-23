230cf5574fcaa634054a7d755a07f6008fc84248
Felipe Gontijo <epilef.gontijo@gmail.com>
2020-10-23 12:06:57 -0300

    feat: Add todolists and relationship

--- app.py
+++ app.py
@@ -14,9 +14,17 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    todolist_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo ID: {self.id} {self.description}>'
+
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True)
+
 
 # db.create_all() --> no need since now we're using Flask-Migrate
 
